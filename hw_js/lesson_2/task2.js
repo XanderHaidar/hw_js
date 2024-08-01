de6478ff38cb21ef6c1996bf2eb173d48fc74a0e
xander <151149803+XanderHaidar@users.noreply.github.com>
2024-08-01 18:14:45 +0300

    Delete hw_js directory

--- hw_js/lesson_2/task2.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-// Вводемо дані
-let yearOfBirth = parseInt(prompt(`Enter the year of birth`, `1989`))
-// Використовуємо метод getFullYear для розрахунку віку на основі введеної дати народження
-let currentYear = new Date().getFullYear()
-// Знаходимо результат
-let age = currentYear - yearOfBirth
-// Виводимо результат
-document.write(`<p>Your age=${age}</p>`)
